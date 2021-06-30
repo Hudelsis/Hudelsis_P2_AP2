7df2b65e40b076b5caaee041dd1d74261ad3b700
Hudelsis Antigua Mendez <antiguamendez27@gmail.com>
2021-07-01 00:18:35 +0200

    Inicial

---
+++ Hudelsis_P2_AP2/BLL/ClientesBLL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hudelsis_P2_AP2.DAL;
+using Hudelsis_P2_AP2.Models;
+using System.Linq.Expressions;
+
+namespace Hudelsis_P2_AP2.BLL
+{
+    public class ClientesBLL
+    {
+        public static List<Clientes> GetList(Expression<Func<Clientes, bool>> cliente)
+        {
+            List<Clientes> Lista = new List<Clientes>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                Lista = contexto.Clientes.Where(cliente).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Lista;
+        }
+    }
+}
