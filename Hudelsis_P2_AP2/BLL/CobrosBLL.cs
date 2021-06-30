b15a5a90d61d92bf4eba87f0b9b22ea815456669
Hudelsis Antigua Mendez <antiguamendez27@gmail.com>
2021-07-01 00:31:35 +0200

    Agregando las BLL

---
+++ Hudelsis_P2_AP2/BLL/CobrosBLL.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hudelsis_P2_AP2.DAL;
+using Hudelsis_P2_AP2.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Hudelsis_P2_AP2.BLL
+{
+    public class CobrosBLL
+    {
+        public static bool Guardar(Cobros cobro)
+        {
+            return Insertar(cobro);
+        }
+
+        private static bool Insertar(Cobros cobro)
+        {
+            bool Insertado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                foreach (var item in cobro.cobrosDetalle)
+                {
+                    item.Venta = contexto.Ventas.Find(item.VentaId);
+                    item.Venta.Balance -= item.Cobrado;
+                    contexto.Entry(item.Venta).State = EntityState.Modified;
+                }
+                contexto.Cobros.Add(cobro);
+                Insertado = (contexto.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Insertado;
+        }
+
+
+        public static bool Eliminar(int id)
+        {
+            bool Eliminado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var cobro = Buscar(id);
+
+                contexto.Entry(cobro).State = EntityState.Deleted;
+                Eliminado = (contexto.SaveChanges() > 0);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Eliminado;
+        }
+
+        public static Cobros Buscar(int id)
+        {
+            Cobros cobro = new Cobros();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                cobro = contexto.Cobros.Where(c => c.CobroId == id).Include(c => c.cobrosDetalle).FirstOrDefault();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return cobro;
+        }
+
+        public static List<Cobros> GetList(Expression<Func<Cobros, bool>> cobro)
+        {
+            List<Cobros> Lista = new List<Cobros>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                Lista = contexto.Cobros.Where(cobro).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Lista;
+        }
+    }
+}
+
